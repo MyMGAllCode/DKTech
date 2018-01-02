7a776eed823a50353aaa9310b63d574613c0bb50
dktech89 <dktech89@gmail.com>
2018-01-02 20:55:21 +0530

    message

---
+++ src/main/java/com/dktech/DkTechApplication.java
@@ -0,0 +1,12 @@
+package com.dktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkTechApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dktech/DkTechApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dktech;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DkTechApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
